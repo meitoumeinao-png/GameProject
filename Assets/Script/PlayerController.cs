f2070d70e33f298d864206f66722f5d2cea84665
meitoumeinao-png <chiayeesheng100@gmail.com>
2026-07-01 17:56:18 +0800

    Week 1 Friday
    Added a flip script based on the moveinput in the movingleftandright to change according via -1 to 1 changing the character orientation.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
+using System.Net;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEditor.U2D.Aseprite;
 using UnityEngine;
@@ -21,7 +23,6 @@ public class PlayerController : MonoBehaviour
     [Header("Dash")]
     public float dashspeed = 50.0f;
     [SerializeField]public float dashduration = 0.5f;
-    private bool dashonce = false;
     private float dashtimer;
     private float maxdashtime;
     private bool isdashing;
@@ -84,11 +85,16 @@ public class PlayerController : MonoBehaviour
             StartCoroutine(Dash());
         }
     }
-    //I dont really like how the player can have multiple airdash with this code,depending if the duration is shortened.
+    //I dont really like how the player can have multiple airdash with this code,depending if the duration is shortened
+    // DO STATE THIS IN LATER COMMIT.
     IEnumerator Dash()
     {
         Debug.Log("CanDash");
-
+        dashtimer += Time.deltaTime;
+        if (dashtimer <= maxdashtime)
+        {
+            yield return null;
+        }
         candash = false;
         isdashing = true;
         float originalGravity = rb.gravityScale;
@@ -99,6 +105,7 @@ public class PlayerController : MonoBehaviour
         isdashing = false;
         yield return new WaitForSeconds(dashduration);
         candash = true;
+        dashtimer = 0f;
 
     }
     //Flip 
