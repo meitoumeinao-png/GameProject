4dad70d6768a815c9208fa744d049b9b511c3dc5
meitoumeinao-png <chiayeesheng100@gmail.com>
2026-07-06 22:46:03 +0800

    Week2 Monday
    Trying to get the Super Jump Function to work . Added new enviromental variables  beneath the Jump Header and rewrote the jump function to try and make it to jump by pressing down and space and charging to jump.Ran into issue with figuring out the order of how to jump.Used https://www.youtube.com/watch?v=j111eKN8sJw and https://www.youtube.com/watch?v=F5XMemcAwmE&t=446s to figure out the issue with collisions.Found out that after a successful merge, the HandleCollision was still there and had to be removed as it kept make isgrounded false

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -17,12 +17,14 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     public float speed = 5.0f;
     public bool right;
-    public float jumpforce = 10.0f;
-    private float jumptimecounter;
-    private float maxjumpforce;
-    private float duration = 10.0f;
+    [Header("Jump")]
+    private bool isjumping = false;
+    public float jumpforce = 2.0f;
+    private float secondspassed;
+    private float maxseconds = 5.0f;
+    private float maxjumpforce = 10.0f;
     [Header("Collision Detection")]
-    [SerializeField] private float groundcheckdistance;
+    private float groundcheckdistance = 3.0f;
     public bool isgrounded;
     [SerializeField]private LayerMask ground;
     [Header("Dash")]
@@ -40,7 +42,7 @@ public class PlayerController : MonoBehaviour
         maxdashtime = dashduration;
     }
 
-
+    //Issue with this is that It only gets it for a frame and then is overwritten
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Platform"))
@@ -52,9 +54,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HandleCollision();
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, Vector2.down*groundcheckdistance, Color.green);
         MovingLeftOrRight();
-        StartCoroutine(SuperJump());
+        Jump();
         ToDash();
     }
 
@@ -74,82 +77,78 @@ public class PlayerController : MonoBehaviour
         Flip(moveinput);
         rb.linearVelocity = new Vector2((speed * moveinput), rb.linearVelocityY);
     }
+ 
+    //I want to have a charging mechanic to increase jumpforce proportional to time while they charge,but i dont want them to jump out of frame.So I guess
+    //I will make it so that as long as they hold down the spacebar, time will build up and by then.it will be seconds/max second * maxjumpforce
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isgrounded == true )
+        if(Input.GetKey(KeyCode.Space) && isgrounded == true)
         {
-            Debug.Log("Jump");
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
-            isgrounded = false;
+            isjumping = true;
+            rb.linearVelocity = new Vector2(rb.linearVelocityX,jumpforce);
+        }
+        while (isgrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            secondspassed =+ Time.fixedDeltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isjumping = false;
+            rb.linearVelocity = new Vector2(rb.linearVelocityX,(secondspassed/maxseconds)*maxjumpforce);
         }
     }
-    //Used CoRountine as this feature will have a cooldown
-    void ToDash()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift) && candash == true)
+        //Used CoRountine as this feature will have a cooldown
+        void ToDash()
         {
-            StartCoroutine(Dash());
+            if (Input.GetKeyDown(KeyCode.LeftShift) && candash == true)
+            {
+                StartCoroutine(Dash());
+            }
         }
-    }
-    //I dont really like how the player can have multiple airdash with this code,depending if the duration is shortened
-    // DO STATE THIS IN LATER COMMIT.
-    IEnumerator Dash()
-    {
-        Debug.Log("CanDash");
-        dashtimer += Time.deltaTime;
-        if (dashtimer <= maxdashtime)
+        //I dont really like how the player can have multiple airdash with this code,depending if the duration is shortened
+        // DO STATE THIS IN LATER COMMIT.
+        IEnumerator Dash()
         {
-            yield return null;
-        }
-        candash = false;
-        isdashing = true;
-        float originalGravity = rb.gravityScale;
-        rb.gravityScale = 0f;
-        rb.linearVelocity = new Vector2(transform.localScale.x * dashspeed, 0f);
-        yield return new WaitForSeconds(dashduration);
-        rb.gravityScale = originalGravity;
-        isdashing = false;
-        yield return new WaitForSeconds(dashduration);
-        candash = true;
-        dashtimer = 0f;
+            Debug.Log("CanDash");
+            dashtimer += Time.deltaTime;
+            if (dashtimer <= maxdashtime)
+            {
+                yield return null;
+            }
+            candash = false;
+            isdashing = true;
+            float originalGravity = rb.gravityScale;
+            rb.gravityScale = 0f;
+            rb.linearVelocity = new Vector2(transform.localScale.x * dashspeed, 0f);
+            yield return new WaitForSeconds(dashduration);
+            rb.gravityScale = originalGravity;
+            isdashing = false;
+            yield return new WaitForSeconds(dashduration);
+            candash = true;
+            dashtimer = 0f;
 
-    }
+        }
     //Collision Detection
-    private void HandleCollision()
-    {
-        isgrounded = Physics2D.Raycast(transform.position, Vector2.down, groundcheckdistance, ground);
-    }
 
-    //Flip 
+    //Flip
     void Flip(float moveinput)
     {
         if (moveinput > 0)
         {
             transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
-
         else
         {
             transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
     }
-    //When I jump the object does not move;Capsule Collision Issue;
-    IEnumerator SuperJump()
+    
+    private void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
-        {
-            isgrounded = false;
-            float time =+ Time.deltaTime;
-            jumptimecounter = time;
-            yield return new WaitForSecondsRealtime(duration);
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            Debug.Log("Jump");
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x,jumpforce * jumptimecounter);
-            jumptimecounter = 0;
-            yield return null;
-        }
-
+        ///if (isgrounded == true)
+        ///{
+           /// Debug.Log("Yes");
+        ///}
     }
+
 }
